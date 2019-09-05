cf32b35f67c8f25b699a5cacc5257c50c63f37d8
Sambit <sambit.mhptr22@gmail.com>
2019-09-05 18:13:20 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+require("./tailwind.default.config.js");
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+      '76': '19rem'}
+		
+	}
+  },
+  variants: {
+
+  
+     //enable active variant for bg color (extending by default)
+    //  backgroundColor: ['responsive', 'hover', 'focus', 'active'], 
+
+ 
+      
+
+    
+  },
+  plugins: []
+}
